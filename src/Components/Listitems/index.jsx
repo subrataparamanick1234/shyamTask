8ddb9caa718f3ee601f95c989828eb8da0bc1e34
subratawebskitters <subrata.paramanick@webskitters.com>
2023-02-03 12:01:23 +0530

    wip

--- src/Components/Listitems/index.jsx
+++ src/Components/Listitems/index.jsx
@@ -2,12 +2,7 @@ import * as React from "react";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import ListSubheader from "@mui/material/ListSubheader";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import PeopleIcon from "@mui/icons-material/People";
-import BarChartIcon from "@mui/icons-material/BarChart";
-import LayersIcon from "@mui/icons-material/Layers";
 
 export const mainListItems = (
   <React.Fragment>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 
 import "react-toastify/dist/ReactToastify.css";
 
